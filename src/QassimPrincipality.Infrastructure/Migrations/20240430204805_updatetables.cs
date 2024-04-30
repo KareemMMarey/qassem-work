213d544111b049640bf704a98e42cfd5db9bd466
kareem <kareem@DESKTOP-J6ITIFI>
2024-04-30 13:49:13 -0700

    commit changes

---
+++ src/QassimPrincipality.Infrastructure/Migrations/20240430204805_updatetables.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QassimPrincipality.Infrastructure.Migrations
+{
+    public partial class updatetables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactTitle",
+                schema: "services",
+                table: "ContactForm",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserFullName",
+                schema: "services",
+                table: "ContactForm",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserMobile",
+                schema: "services",
+                table: "ContactForm",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "EntityType",
+                schema: "lookup",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    NameAr = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NameEn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntityType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RequesterType",
+                schema: "lookup",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    NameAr = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NameEn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RequesterType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShareDataRequest",
+                schema: "services",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserFullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IdentityNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserMobile = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EntityTypeId = table.Column<int>(type: "int", nullable: false),
+                    EntityName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PurposeOfRequest = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsShareAgreementExist = table.Column<bool>(type: "bit", nullable: true),
+                    IsContainsPersonalData = table.Column<bool>(type: "bit", nullable: true),
+                    IsRequesterDataOfficePresenter = table.Column<bool>(type: "bit", nullable: true),
+                    IsLegalJustification = table.Column<bool>(type: "bit", nullable: true),
+                    LegalJustificationDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShareDataRequest", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShareDataRequest_EntityType_EntityTypeId",
+                        column: x => x.EntityTypeId,
+                        principalSchema: "lookup",
+                        principalTable: "EntityType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OpenDataRequest",
+                schema: "services",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserFullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserMobile = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RequesterTypeId = table.Column<int>(type: "int", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UpdatedOn = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OpenDataRequest", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OpenDataRequest_RequesterType_RequesterTypeId",
+                        column: x => x.RequesterTypeId,
+                        principalSchema: "lookup",
+                        principalTable: "RequesterType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OpenDataRequest_RequesterTypeId",
+                schema: "services",
+                table: "OpenDataRequest",
+                column: "RequesterTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShareDataRequest_EntityTypeId",
+                schema: "services",
+                table: "ShareDataRequest",
+                column: "EntityTypeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OpenDataRequest",
+                schema: "services");
+
+            migrationBuilder.DropTable(
+                name: "ShareDataRequest",
+                schema: "services");
+
+            migrationBuilder.DropTable(
+                name: "RequesterType",
+                schema: "lookup");
+
+            migrationBuilder.DropTable(
+                name: "EntityType",
+                schema: "lookup");
+
+            migrationBuilder.DropColumn(
+                name: "ContactTitle",
+                schema: "services",
+                table: "ContactForm");
+
+            migrationBuilder.DropColumn(
+                name: "UserFullName",
+                schema: "services",
+                table: "ContactForm");
+
+            migrationBuilder.DropColumn(
+                name: "UserMobile",
+                schema: "services",
+                table: "ContactForm");
+        }
+    }
+}
