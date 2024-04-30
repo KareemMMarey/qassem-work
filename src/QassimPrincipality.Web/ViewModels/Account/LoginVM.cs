0c959e5c1c202edcf5ac2ca70388b8b4e3a464a5
E-Kareem Marey <e-k.marey@dsc.gov.sa>
2024-04-30 18:19:59 +0300

    commit changes

---
+++ src/QassimPrincipality.Web/ViewModels/Account/LoginVM.cs
@@ -0,0 +1,8 @@
+﻿namespace QassimPrincipality.Web.ViewModels.Account
+{
+    public class LoginVM
+    {
+        public  string EmailAddress { get; set; }
+        public string Password { get; set; }
+    }
+}
