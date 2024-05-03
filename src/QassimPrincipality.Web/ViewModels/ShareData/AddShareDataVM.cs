2192e506dcd44e4053cb045bbc386307cfa593f5
kareem <kareem@DESKTOP-J6ITIFI>
2024-05-03 13:34:21 -0700

    commait changes

---
+++ src/QassimPrincipality.Web/ViewModels/ShareData/AddShareDataVM.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QassimPrincipality.Web.ViewModels.ShareData
+{
+    public class AddShareDataVM
+    {
+        public string UserFullName { get; set; }
+
+        [Required(ErrorMessage = "أدخل البريد الإلكتروني")]
+        public string UserEmail { get; set; }
+        [Required(ErrorMessage = "أدخل  الجوال")]
+        public string UserMobile { get; set; }
+        [Required(ErrorMessage = "أدخل عنوان النموذج")]
+        public string ContactTitle { get; set; }
+        [Required(ErrorMessage = "أدخل نص الشكوى ")]
+        public string Description { get; set; }
+        public string IdentityNumber { get; set; }
+        public int ContactTypeId { get; set; }
+        public bool IsApproved { get; set; }
+    }
+}
