0292e9e3a6f19a90d79443331a76324ff640facc
Mahmoud Qassem <mqassem.c@NEC.GOV.SA>
2025-06-04 20:47:04 +0300

    SAVE INFO DONE

---
+++ src/framework/Framework.Identity/Data/Dtos/ContactInfoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Framework.Identity.Data.Dtos
+{
+    public class ContactInfoDto
+    {
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Id { get; set; }
+    }
+}
